a92293db112a2d2ea5a4fe28ce9a240da483ac3b
Tuan Truong <tuan188@gmail.com>
2017-05-11 11:19:09 +0700

    Add library, resource

---
+++ iOSTemplateProject/Libraries/SampleLibrary.swift
@@ -0,0 +1,13 @@
+//
+//  SampleLibrary.swift
+//  iOSTemplateProject
+//
+//  Created by Tuan Truong on 5/11/17.
+//  Copyright © 2017 Framgia. All rights reserved.
+//
+
+import UIKit
+
+class SampleLibrary: NSObject {
+
+}
---
+++ iOSTemplateProject/Resources/SampleResource.swift
@@ -0,0 +1,13 @@
+//
+//  SampleResource.swift
+//  iOSTemplateProject
+//
+//  Created by Tuan Truong on 5/11/17.
+//  Copyright © 2017 Framgia. All rights reserved.
+//
+
+import UIKit
+
+class SampleResource: NSObject {
+
+}
